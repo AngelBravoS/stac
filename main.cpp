4082630f6ee7afef1856597fe4def7cf6c8a96b5
Ángel Bravo Sáenz <angelbravosaenz@gmail.com>
2017-03-06 20:55:17 +0100

    Primer commit. Hola mundo de prueba

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
+int main () {
+    std::cout << "Hola mundo." << std::endl;
     return 0;
-}
+    }
